989af944b0a1cea66b510ceca53ee20138019271
huicpc0215 <huicpc0215@gmail.com>
2014-09-19 14:46:17 +0800

    edit exercise 2 and shell script

--- Exercise2/solution.m
+++ Exercise2/solution.m
@@ -3,7 +3,8 @@ y = load('./ex2y.dat');
 plot(x,y,'o');
 ylabel('Height in meters')
 xlabel('Age in years')
-x = [ones(1,length(y)),x];
+m = length(y);
+x = [ones(m,1),x];
 j_vals = zeros(100,100);
 theta0_vals = linspace(-3,3,100);
 theta1_vals = linspace(-1,1,100);
@@ -16,12 +17,22 @@ end
 theta_val=[0,0];
 a=0.07;
 theta_val = theta_val - a *0.02 *( (x*theta_val')'-y')*x;
-%for i = 1 : 2000
+for i = 1 : 2000
+    theta_val = theta_val - a *0.02 *( (x*theta_val')'-y')*x;
+end
+
+nx=linspace(2,8,1000);
+ny=ones(1,1000)*theta_val(1)+nx*theta_val(2);
+plot(nx,ny,'o');
+hold on;
+plot(x,y,'o');
+ylabel('Height in meters')
+xlabel('Age in years')
 
 %plot the surface plot
 j_vals = j_vals';
 figure;
 surf(theta0_vals,theta1_vals,j_vals);
-xlabel('\theta0');
-ylabel('\theta1');
+xlabel('\theta0')
+ylabel('\theta1')
 
